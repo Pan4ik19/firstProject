7ab56c1a01b124fc1de142cf6998145bdcbeb803
Pan4ik19 <panov19rus1@gmail.com>
2023-12-13 22:42:26 +0700

    Метод Post, валидация данных

--- app/public/post_registrate.php
+++ app/public/post_registrate.php
@@ -1,13 +1,100 @@
 <?php
 
-print_r($_GET);
-
-$pdo = new PDO("pgsql:host=db;dbname=postgres", "testuser", "qwerty");
-
-$name = $_GET['name'];
-$email = $_GET['email'];
-$password = $_GET['psw'];
+print_r($_POST);
 
 
 
-$pdo->exec("insert into users(name, email, password) values('$name','$email','$password')");
+
+class UserRegistration
+{
+    private $name;
+    private $email;
+    private $password;
+
+    public function __construct($name, $email, $password)
+    {
+        $this -> name = $this->validateName($name);
+        $this -> email = $this->validateEmail($email);
+        $this -> password = $this->validatePassword($password);
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+
+    public function validateName($name)
+    {
+        $str = strlen($name);
+        $pattern = "/^[a-zA-z]*$/";
+        $flag = 0;
+        if ($str == 0 && $str < 2 ){
+            echo "Error! You didn't enter the Name.";
+            $flag = 1;
+        }elseif (!preg_match ($pattern, $name)){
+            echo "Error! You didn't enter the Name.";
+        }else {
+            return $name;
+        }
+
+
+    }
+
+    public function validateEmail($email)
+    {
+        $pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";
+        if (!preg_match ($pattern, $email)){
+            echo "Email is not valid.";
+        } else {
+            return $email;
+        }
+    }
+
+    public function validatePassword($password)
+    {
+        $len = strlen($password);
+        if ($len < 4)
+        {
+            echo "Password is not valid";
+        } else {
+            return $password;
+        }
+    }
+
+
+}
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['psw'];
+
+$user = new UserRegistration($name, $email, $password);
+
+if ($user->getName() !== Null  && $user->getEmail() !== Null  && $user->getPassword() !== Null)
+{
+    $pdo = new PDO("pgsql:host=db;dbname=postgres", "testuser", "qwerty");
+    $name = $user->getName();
+    $email = $user->getEmail();
+    $password = $user->getPassword();
+    $pdo->exec("insert into users(name, email, password) values('$name','$email','$password')");
+    echo "Nice";
+}
+
+
+
+
+
+
